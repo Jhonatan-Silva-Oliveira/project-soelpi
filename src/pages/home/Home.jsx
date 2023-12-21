b88cdd34adf97c0d2da5366ff904190c09eef556
JhonatanSO <jhonerex2012@gmail.com>
2023-12-21 09:22:35 -0300

    modificado estrutura de pasta, criado banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,12 @@
-import { HomeContainer, LimitContainer, BtnBanner, ContainerLogo, ContainerPlatforms } from './Home.style';
-
-import logo from "../../assets/img/logos/logo-Soelpi.svg";
-import defaultQr from "../../assets/img/platforms/default-qr.png";
-import imgAppStore from "../../assets/img/platforms/img-appStore.png";
-import imgEpicStore from "../../assets/img/platforms/img-epicStore.png";
-import imgGooglePlay from "../../assets/img/platforms/img-googlePlay.png";
-import imgPs5 from "../../assets/img/platforms/img-ps5.png";
-import imgWindows from "../../assets/img/platforms/img-windows.png";
+import Banner from "./banner/Banner"
 
 
 const Home = () =>
 {
   return (
-    <HomeContainer>
-      <LimitContainer>
-        <section className="banner">
-          <ContainerLogo>
-            <img className='logo-soelpi' src={logo} alt="logo" />
-          </ContainerLogo>
-          <BtnBanner>Fazer pré-registro</BtnBanner>
-          <ContainerPlatforms>
-            <p className='paragraph-platporms'>Em breve nas plataformas</p>
-            <div className='container-img-platforms'>
-              <img className='img-platform' src={defaultQr} alt="qr" />
-              <img className='img-platform' src={imgPs5} alt="Ps5" />
-              <img className='img-platform' src={imgWindows} alt="Wins" />
-              <img className='img-platform' src={imgAppStore} alt="AppS" />
-              <img className='img-platform' src={imgGooglePlay} alt="Play" />
-              <img className='img-platform' src={imgEpicStore} alt="Epic" />
-            </div>
-          </ContainerPlatforms>
-        </section>
-      </LimitContainer>
-    </HomeContainer>
+    <div>
+      <Banner />
+    </div>
   )
 }
 

45fd4c1f8d48755079211fe97e81ed0dea53b0d3
JhonatanSO <jhonerex2012@gmail.com>
2023-12-22 18:38:32 -0300

    secao word home finalizada

--- src/pages/home/word-home/WordHome.jsx
+++ src/pages/home/word-home/WordHome.jsx
@@ -1,27 +1,64 @@
-import { NewsContainer, LimitContainer } from './WordHome.style';
+import { WordContainer, LimitContainer, ContainWordHome, ContainerImgWord } from './WordHome.style';
+
+import { useEffect, useRef } from 'react';
+import VanillaTilt from 'vanilla-tilt';
+import PropTypes from 'prop-types';
+
+import imgFlorest from "../../../assets/img/places/floresta-home.png";
+import imgCampo from "../../../assets/img/places/planice-home.png";
+import imgCastelo from "../../../assets/img/places/casteloHome.png";
+
+function Tilt(props)
+{
+  const { options, ...rest } = props;
+  const tilt = useRef(null);
+
+  useEffect(() =>
+  {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
+  Tilt.propTypes = {
+    options: PropTypes.object.isRequired,
+  };
+
+  return <div ref={tilt} {...rest} />;
+}
 
 const WordHome = () =>
 {
-  return (
-    <NewsContainer>
-      <LimitContainer>
-        <div>
-          <h2>Explore um mundo repleto de aventuras
-            e magia</h2>
-          <p>Desbravando um mundo vasto, explore por florestas cheias de magia, deslumbre a vista de planícies, desvende os segredos ocultos de um mundo cheio de aventura e mistérios.</p>
-          <a href="#">Mais Informações</a>
-          <div>
-            <img src="#" alt="" />
-            <img src="#" alt="" />
-          </div>
-        </div>
+  const options = {
+    scale: 1.1,
+    speed: 2000,
+    max: 5,
+    glare: true,
+    "max-glare": .1,
+  };
 
-        <div>
-          <img src="" alt="" />
-        </div>
+  return (
+    <WordContainer>
+      <LimitContainer>
+        <ContainWordHome>
+          <h2 className="title">Explore um mundo repleto de aventuras
+            e magia</h2>
+          <p className="description">Desbravando um mundo vasto, explore por florestas cheias de magia, deslumbre a vista de planícies, desvende os segredos ocultos de um mundo cheio de aventura e mistérios.</p>
+          <a className="btn-more-info" href="#">Mais Informações</a>
+          <div className='container-img-words'>
+            <img src={imgFlorest} alt="floresta" />
+            <img src={imgCampo} alt="campo" />
+          </div>
+        </ContainWordHome>
+
+        <ContainerImgWord>
+          <div className="box-image" >
+            <Tilt options={options}>
+              <img src={imgCastelo} alt="castelo" />
+            </Tilt>
+          </div>
+        </ContainerImgWord>
 
       </LimitContainer>
-    </NewsContainer>
+    </WordContainer>
   )
 }
 
--- src/pages/home/word-home/WordHome.style.jsx
+++ src/pages/home/word-home/WordHome.style.jsx
@@ -2,59 +2,104 @@ import styled from "styled-components";
 
 import { containerStyles } from "../../../assets/styles/SharedStyles";
 
-import angelBG from "../../../assets/img/update/princess-angel-bg.png";
 
-export const NewsContainer = styled.div`
+export const WordContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  padding: 80px 0;
   position: relative;
-  
-  &::after{
-    z-index: -1;
-    position: absolute;
-    content: '';
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: #00000073;
-  }
-  &::before{
-    z-index: -2;
-    position: absolute;
-    content: '';
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: url(${angelBG});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: top;
-  }
+  background-color: black;
 `
 export const LimitContainer = styled.div`
   ${containerStyles}
+  position: relative;
   height: 100%;
-  padding: 150px 0;
 `
-export const ContainerTitle = styled.div`
+export const ContainWordHome = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
-  align-items: center;
-  gap: 20px;
-  margin-bottom: 60px;
   
   & .title{
-    text-transform: uppercase;
+    width: 16ch;
     font-family: ${({ theme }) => theme.family.messi};
-    font-weight: ${({ theme }) => theme.weight.medium};
+    font-weight: ${({ theme }) => theme.weight.regular};
     font-size: ${({ theme }) => theme.sizes.xxl};
     color: ${({ theme }) => theme.colors.white};
+    line-height: 58px;
+    margin-bottom: 32px;
   }
+  & .description{
+    width: 38ch;
+    font-family: ${({ theme }) => theme.family.robo};
+    font-weight: ${({ theme }) => theme.weight.regular};
+    font-size: ${({ theme }) => theme.sizes.m};
+    color: ${({ theme }) => theme.colors.textColorLight};
+    line-height: 30px;
+    margin-bottom: 10px;
+  }
+  & .btn-more-info{
+    display: inline-block;
+    width: fit-content;
+    text-transform: uppercase;
+    font-family: ${({ theme }) => theme.family.pop};
+    font-weight: ${({ theme }) => theme.weight.medium};
+    font-size: ${({ theme }) => theme.sizes.m};
+    color: ${({ theme }) => theme.colors.white};
+    padding: 2px 0;
+    text-decoration: none;
+    border-bottom: 2px solid ${({ theme }) => theme.colors.white};
+    margin-bottom: 40px;
+    transition: all .2s;
+    
+    &:hover{
+      transform: translate(5px);
+    }
+  }
+  & .container-img-words{
+    display: flex;
+    gap: 40px;
+    
+    & img{
+      display: inline-block;
+      width: 240px;
+    }
+  }
+`
+export const ContainerImgWord = styled.div`
+  position: absolute;
+  right: 20px;
+  top: -110px;
   
-  & .title-detail{
-    object-fit: cover;
-    width: 500px;
+  & .box-image{
+    border: 5px inset ${({ theme }) => theme.colors.black};
+    width: 550px;
+    height: 800px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    
+    &::before{
+      z-index: 1;
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 100%;
+      background: transparent;
+      pointer-events: none;
+      top: 0;
+      left: 0;
+      -webkit-box-shadow: inset 0px 0px 30px 10px rgba(0,0,0,0.75);
+      -moz-box-shadow: inset 0px 0px 30px 10px rgba(0,0,0,0.75);
+      box-shadow: inset 0px 0px 30px 10px rgba(0,0,0,0.75);
+    }
+    
+    & img{
+      width: 600px;
+      height: 900px;
+      object-fit: cover;
+      object-position: 50%;
+    }
   }
 `
